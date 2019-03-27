9c79319af3b3d84b727dd8d042ba974b8cd7f324
Danny Gauthier <danny.m.gauthier@gmail.com>
2019-03-27 15:18:03 -0400

    added features on the lead model

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -23,11 +23,23 @@ class Lead < ApplicationRecord
             config.logger = Logger.new(STDOUT)
         
         end
+
+        if self.attached_file
+            attached = "The user attached a file"
+        else
+            attached = ""
+        end
+
+        if self.message
+            mes = "Attached Message: #{self.message}"
+        else
+            mes = "Attached Message: None"
+        end
       
         client.tickets.create!(:subject => "#{self.full_name} from #{self.business_name}", :type => "question",
         :comment => { :value => "The contact #{self.full_name} from company #{self.business_name} can be reached at email #{self.email} and at phone number #{self.phone_number}.
         #{self.department_in_charge_of_elevators} has a project named #{self.project_name} which would require contribution from Rocket Elevators.
-        \n \n #{self.project_description} \n \n Attached Message: #{self.message}"},
+        \n \n #{self.project_description} \n \n #{mes} \n \n #{attached}"},
         :submitter_id => client.current_user.id, :priority => "normal")
         
     end
