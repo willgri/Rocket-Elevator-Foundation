ec8635bd9eebaf2a61a747c7ff68a99c6fa4c3ad
Dominic Villemure <doumvil@hotmail.com>
2019-03-26 20:12:53 -0400

    added text to speech with watson

--- app/controllers/watson_controller.rb
+++ app/controllers/watson_controller.rb
@@ -5,38 +5,123 @@ require 'uri'
 require 'openssl'
 include IBMWatson
 class WatsonController < ApplicationController
+    
 
     def watson
-        uri = URI.parse("https://iam.bluemix.net/identity/token")
+
+        firstName = current_admin['first_name']
+        numberElevator = Elevator.count.to_s
+        numberBuilding = Building.count.to_s
+        numberCustomer = Customer.count.to_s
+        numberInactiveElevator = Elevator.where(status: 'Inactive').count.to_s
+        numberQuote = Quote.count.to_s
+        numberLead = Lead.count.to_s
+        numberBattery = Battery.count.to_s
+        numberCity = Address.distinct(:city).count.to_s
+        
+
+        
+        # select("status").count
+        
+        p(numberCity)
+     
+
+        uri = URI.parse("https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize")
         request = Net::HTTP::Post.new(uri)
-        request.content_type = "application/x-www-form-urlencoded"
-        request["Accept"] = "application/json"
-        request.set_form_data(
-            "apikey" => "i3q-dboh27eQRf3gipo5CT9VVqF0ZE2yAbJhvWI8sjDG",
-            "grant_type" => "urn:ibm:params:oauth:grant-type:apikey",
-           )
+        request.basic_auth("apikey", "i3q-dboh27eQRf3gipo5CT9VVqF0ZE2yAbJhvWI8sjDG")
+        request.content_type = "application/json"
+        request["Accept"] = "audio/ogg"
+        request.body = JSON.dump({
+        "text" => "Greetings," + firstName + "they are currently," + numberElevator + 
+        ",elevators deployed in the," + numberBuilding + ",buildings of your," + numberCustomer +
+        ",customers. Currently," + numberInactiveElevator + ",elevators are in status inactive and being serviced.
+        You currently have," + numberQuote + ",quotes awaiting processing. You currently have," +
+        numberLead + ",leads in your contact requests. " + numberBattery + ",batteries are deployed across," + 
+        numberCity + ",cities."
+
+
+
+        })
 
         req_options = {
         use_ssl: uri.scheme == "https",
-        verify_mode: OpenSSL::SSL::VERIFY_NONE,
         }
 
         response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
         http.request(request)
-            end
+        end
 
-        token = JSON.parse(response.body)
-        token1 = token['access_token']
-        voice = "en-US_AllisonVoice"
-        text = "Hello world"
-        p (token)
-        render :json => {text: text, token: token1, voice: voice}
+        send_data response.body
+    
 
-        File.open("hello_world.wav", "wb") do |audio_file|
-        response = token.synthesize(
-        )
-        audio_file.write(response.body)
     end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # uri = URI.parse("https://iam.bluemix.net/identity/token")
+        # request = Net::HTTP::Post.new(uri)
+        # request.content_type = "application/x-www-form-urlencoded"
+        # request["Accept"] = "application/json"
+        # request.set_form_data(
+        #     "apikey" => "i3q-dboh27eQRf3gipo5CT9VVqF0ZE2yAbJhvWI8sjDG",
+        #     "grant_type" => "urn:ibm:params:oauth:grant-type:apikey",
+        #    )
+
+        # req_options = {
+        # use_ssl: uri.scheme == "https",
+        # verify_mode: OpenSSL::SSL::VERIFY_NONE,
+        # }
+
+        # response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        # http.request(request)
+        #     end
+
+        # token = JSON.parse(response.body)
+        # token1 = token['access_token']
+        # voice = "en-US_AllisonVoice"
+        # text = "Hello world"
+        # p (token)
+        # render :json => {text: text, token: token1, voice: voice}
+
+        # File.open("hello_world.wav", "wb") do |audio_file|
+        # response = token.synthesize(
+        # )
+        # audio_file.write(response.body)
+   
       
 
 # response.code
@@ -117,7 +202,7 @@ class WatsonController < ApplicationController
     # end
     # response.code
     # response.body
-    end
+    # end
 end
 
 
