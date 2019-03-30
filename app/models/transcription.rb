41b074357cd4f487dea70465103559fe294a1699
Danny Gauthier <danny.m.gauthier@gmail.com>
2019-03-29 22:20:07 -0400

    cleaned up transcription.rb

--- app/models/transcription.rb
+++ app/models/transcription.rb
@@ -15,7 +15,6 @@ class Transcription < ApplicationRecord
 
         the_mp3 = self.transcription_file
 
-    # client = Aws::EC2::Client.new(region:'us-west-2', credentials: credentials)
     tclient = Aws::TranscribeService::Client.new(
         region: 'us-east-2',
         access_key_id: 'AKIAJWCNKX5L676W4QBQ',
@@ -28,25 +27,15 @@ class Transcription < ApplicationRecord
         secret_access_key: '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'
     )
   
-      #File.open('/home/pocketpapa/Audio/testaudio.mp3', 'rb') do |file|
         sclient.put_object(bucket: 'hugeaudio', key: "#{the_time.strftime "%Y-%m-%d %H:%M"}.mp3", body: "#{the_mp3}")
-      #end
-  
-      # # resp = sclient.put_object({
-      # #   body: "testaudio.mp3", 
-      # #   bucket: "hugeaudio", 
-      # #   key: "2019-03-29-09ddd.mp3", 
-      # #   # tagging: "key1=value1&key2=value2", 
-      # # })
 
       the_job = the_time.strftime "%Y_%m_%d_%H_%M"
   
       resp = tclient.start_transcription_job({
-        transcription_job_name: "#{the_job}_JOB", # required
-        language_code: "en-US", # required, accepts en-US, es-US, en-AU, fr-CA, en-GB, de-DE, pt-BR, fr-FR, it-IT, ko-KR
-        # media_sample_rate_hertz: 1,
-        media_format: "mp3", # required, accepts mp3, mp4, wav, flac
-        media: { # required
+        transcription_job_name: "#{the_job}_JOB",
+        language_code: "en-US",
+        media_format: "mp3",
+        media: {
           media_file_uri: "https://s3.us-east-2.amazonaws.com/hugeaudio/#{the_time.strftime "%Y-%m-%d %H:%M"}.mp3",
         },
       })
@@ -55,7 +44,7 @@ class Transcription < ApplicationRecord
         attempt = 1
         WaitUtil.wait_for_condition('my event', :verbose => true, :timeout_sec => 5000, :delay_sec => 10) do
           transcribe = tclient.get_transcription_job({
-            transcription_job_name: "#{the_job}_JOB", # required
+            transcription_job_name: "#{the_job}_JOB",
           })
           transcribe_status = transcribe.transcription_job.transcription_job_status
           puts transcribe_status
